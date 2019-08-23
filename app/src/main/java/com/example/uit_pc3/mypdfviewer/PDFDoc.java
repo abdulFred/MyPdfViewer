145b49c0f17224bef7416a6f76a261fd3bd761f6
abdulFred <abdullaevfarhodjon@gmail.com>
2019-08-23 09:33:40 +0900

    First commit

---
+++ app/src/main/java/com/example/uit_pc3/mypdfviewer/PDFDoc.java
@@ -0,0 +1,22 @@
+package com.example.uit_pc3.mypdfviewer;
+public class PDFDoc {
+    String name,path;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
